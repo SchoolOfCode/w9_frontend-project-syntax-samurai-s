fe87fc704ff6a13a77a1b08dd42aaa58fe9b470c
DevMattDavies <mdavies.me@pm.me>
2022-06-21 19:47:40 +0100

    Dynamic searchbar operational

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -2,45 +2,28 @@ import { useState } from "react";
 import { useEffect } from "react";
 import snippetJSON from "../../db.json";
 import Searchbox from "../Searchbox/Searchbox";
-import SearchResults from "../SearchResult/SearchResult";
 import "./App.css";
 
 function App() {
-  const [search, setSearch] = useState("");
+  const [searchArray, setSearchArray] = useState([]);
 
-  function onChange(e) {
+ 
+  function onClick(e) {
     e.preventDefault();
-    let searchString = e.target.value;
-    searchResults(searchString);
+    console.log('clicked')
   }
 
-  function searchResults(searchString) {
-    // map over JSON data
-    snippetJSON.snippets.map((entry) => {
-      // if no match found return error/info text
-      if (
-        entry.title.toLowerCase().includes(searchString.toLowerCase()) === false
-      ) {
-        return `Your search ${searchString} did not return any results`;
-      } else if (
-        entry.title.toLowerCase().includes(searchString.toLowerCase()) === true
-      ) {
-        // set setSearch to title
-        return setSearch(entry.title);
-        // <p className="search-results">{search}</p>
-        // produce a component with title name
-      }
-    });
-  }
+ 
+
+
 
   return (
-    <div>
-      <Searchbox onChange={onChange} />
-      <SearchResults search={search} />
+    <div id="container">
+      <Searchbox placeholder="Enter a search query..." data={snippetJSON.snippets} onClick={onClick} />
     </div>
   );
 }
 
 export default App;
 
-// 
+//
--- src/Components/Searchbox/Searchbox.js
+++ src/Components/Searchbox/Searchbox.js
@@ -1,7 +1,61 @@
 import "./Searchbox.modules.css";
+import { useState } from "react";
+import SearchIcon from "@mui/icons-material/Search";
+import CloseIcon from "@mui/icons-material/Close";
 
-function Searchbox({ onChange }) {
-  return <input id="searchbox" placeholder="Enter search here..." onChange={onChange}></input>;
+function Searchbox({ placeholder, data, onClick }) {
+  const [filteredData, setFilteredData] = useState([]);
+  const [wordEntered, setWordEntered] = useState("");
+
+  const handleFilter = (e) => {
+    const searchWord = e.target.value;
+    setWordEntered(searchWord);
+    const newFilter = data.filter((value) => {
+      return value.title.toLowerCase().includes(searchWord.toLowerCase());
+    });
+    if (searchWord === "") {
+      setFilteredData([]);
+    } else {
+      setFilteredData(newFilter);
+    }
+  };
+
+  const clearInput = () => {
+    setFilteredData([]);
+    setWordEntered("");
+  }
+
+  return (
+    <div className="search">
+      <div className="searchInputs">
+        <input
+          id="searchbox"
+          type="text"
+          value={wordEntered}
+          placeholder={placeholder}
+          onChange={handleFilter}
+        />
+        <div className="searchIcon">
+          {filteredData.length === 0 ? (
+            <SearchIcon fontSize="large" />
+          ) : (
+            <CloseIcon id="clearBtn" fontSize="large" onClick={clearInput} />
+          )}
+        </div>
+      </div>
+      {filteredData.length !== 0 && (
+        <div className="dataResult">
+          {filteredData.map((value, key) => {
+            return (
+              <a className="dataItem" onClick={onClick}>
+                <p>{value.title}</p>
+              </a>
+            );
+          })}
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default Searchbox;
