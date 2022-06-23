4384013c6ba225c6b7e254545bb1db6d3c75fd5a
Wisteria Hawthorn <92452890+wisteria-hawthorn@users.noreply.github.com>
2022-06-23 11:14:35 +0100

    form component basics complete

--- src/Components/UserSubmit/Form/Form.js
+++ src/Components/UserSubmit/Form/Form.js
@@ -4,12 +4,21 @@ function Form() {
   return (
     <form id="submit-form">
       <label id="submit-title">Title:
-      <input type="text" name="name" />
+      <input type="text" name="title" />
       </label>
-      <label id="submit-title">Code Snippet:
-      <textarea rows="4" cols="50"  type="text" name="name" />
+      <label id="submit-snippet">Code Snippet:
+      <textarea rows="4" cols="50"  type="text" name="snippet" />
       </label>
-      {/* <input type="submit" value="Submit" /> */}
+      <label id="submit-description">Description:
+      <textarea rows="2" cols="50"  type="text" name="description" />
+      </label>
+      <label id="submit-video">Youtube Link:
+      <input type="text" name="youtube-link" />
+      </label>
+      <label id="submit-docs">Docs Link:
+      <input type="text" name="docs-link" />
+      </label>
+      <input id="submit-button" type="submit" value="Submit Snippet" />
     </form>
   );
 }
