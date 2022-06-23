e931dac212c4724f5a71ed2d2d961968fbcafe95
Wisteria Hawthorn <92452890+wisteria-hawthorn@users.noreply.github.com>
2022-06-23 11:46:15 +0100

    flex started on form

--- src/Components/UserSubmit/Form/Form.js
+++ src/Components/UserSubmit/Form/Form.js
@@ -3,22 +3,24 @@ import "./Form.modules.css";
 function Form() {
   return (
     <form id="submit-form">
-      <label id="submit-title">Title:
+    <div id="styling-container">
+      <label className="form-labels" id="submit-title">Title:
       <input type="text" name="title" />
       </label>
-      <label id="submit-snippet">Code Snippet:
+      <label className="form-labels" id="submit-snippet">Code Snippet:
       <textarea rows="4" cols="50"  type="text" name="snippet" />
       </label>
-      <label id="submit-description">Description:
+      <label className="form-labels" id="submit-description">Description:
       <textarea rows="2" cols="50"  type="text" name="description" />
       </label>
-      <label id="submit-video">Youtube Link:
+      <label className="form-labels" id="submit-video">Youtube Link:
       <input type="text" name="youtube-link" />
       </label>
-      <label id="submit-docs">Docs Link:
+      <label className="form-labels" id="submit-docs">Docs Link:
       <input type="text" name="docs-link" />
       </label>
       <input id="submit-button" type="submit" value="Submit Snippet" />
+      </div>
     </form>
   );
 }
