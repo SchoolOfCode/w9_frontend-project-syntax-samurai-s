37fd425b5339911c8c3bb8041ddfcb84e4289983
CJ Chapman <esvagaming@gmail.com>
2022-06-30 07:10:11 +0100

    Removed type module from package json as this was causing complete failure of the app upon using npm start, also created a branch called cjCypressTest and have cypress installed on that branch

--- src/Components/Searchbox/Searchbox.js
+++ src/Components/Searchbox/Searchbox.js
@@ -1,81 +1,80 @@
-import "./Searchbox.modules.css";
-import instance from "../../axiosConfig";
-import axios from "axios";
-import SearchIcon from "@mui/icons-material/Search";
-import CloseIcon from "@mui/icons-material/Close";
+import './Searchbox.modules.css';
+import axios from 'axios';
+import SearchIcon from '@mui/icons-material/Search';
+import CloseIcon from '@mui/icons-material/Close';
 
 function Searchbox({
-  placeholder,
-  resultClick,
-  filteredData,
-  setFilteredData,
-  setSearch,
-  wordEntered,
-  setWordEntered,
+	placeholder,
+	resultClick,
+	filteredData,
+	setFilteredData,
+	setSearch,
+	wordEntered,
+	setWordEntered,
 }) {
-  const handleFilter = (e) => {
-    const searchWord = e.target.value;
-    console.log(searchWord);
-    setWordEntered(searchWord);
-    axios
-      .get(`http://localhost:3000/snippets?title=${searchWord}`)
-      .then((response) => {
-        console.log(response.data.data);
-        setFilteredData(response.data.data);
-      });
-    const newFilter = filteredData.map((value) => {
-      return value.title.toLowerCase().includes(searchWord.toLowerCase());
-    });
-    if (searchWord === "") {
-      setFilteredData([]);
-    } else {
-      setFilteredData(newFilter);
-    }
-  };
+	const handleFilter = e => {
+		const searchWord = e.target.value;
+		console.log(searchWord);
+		setWordEntered(searchWord);
+		axios
+			.get(`http://localhost:3000/snippets?title=${searchWord}`)
+			.then(response => {
+				console.log(response.data.data);
+				setFilteredData(response.data.data);
+			});
+		const newFilter = filteredData.map(value => {
+			return value.title.toLowerCase().includes(searchWord.toLowerCase());
+		});
+		if (searchWord === '') {
+			setFilteredData([]);
+		} else {
+			setFilteredData(newFilter);
+		}
+	};
 
-  const clearInput = () => {
-    setFilteredData([]);
-    setWordEntered("");
-  };
+	const clearInput = () => {
+		setFilteredData([]);
+		setWordEntered('');
+	};
 
-  return (
-    <div className="search">
-      <div className="searchInputs">
-        <input
-          id="searchbox"
-          type="text"
-          value={wordEntered}
-          placeholder={placeholder}
-          onChange={handleFilter}
-        />
-        <div className="searchIcon">
-          {filteredData.length === 0 ? (
-            <SearchIcon fontSize="large" />
-          ) : (
-            <CloseIcon id="clearBtn" fontSize="large" onClick={clearInput} />
-          )}
-        </div>
-      </div>
-      {filteredData.length !== 0 && (
-        <div className="dataResult">
-          {filteredData.map((value, key) => {
-            return (
-              <div className="eachResult">
-                <a
-                  href="#"
-                  key={key}
-                  className="dataItem"
-                  onClick={resultClick}
-                >
-                  <p>{value.title}</p>
-                </a>
-              </div>
-            );
-          })}
-        </div>
-      )}
-    </div>
-  );
+	return (
+		<div className="search">
+			<div className="searchInputs">
+				<input
+					id="searchbox"
+					type="text"
+					value={wordEntered}
+					placeholder={placeholder}
+					onChange={handleFilter}
+				/>
+				<div className="searchIcon">
+					{filteredData.length === 0 ? (
+						<SearchIcon fontSize="large" />
+					) : (
+						<CloseIcon id="clearBtn" fontSize="large" onClick={clearInput} />
+					)}
+				</div>
+			</div>
+			{filteredData.length !== 0 && (
+				<div className="dataResult">
+					{filteredData.map((value, key) => {
+						return (
+							<div className="eachResult">
+								<a
+									href="#"
+									key={key}
+									className="dataItem"
+									onClick={resultClick}
+								>
+									<p>{value.title}</p>
+								</a>
+							</div>
+						);
+					})}
+				</div>
+			)}
+		</div>
+	);
 }
 
 export default Searchbox;
